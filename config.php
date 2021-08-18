91f28cc43ea433ce832f1ada0a8bff0e026e2ec0
mohsinlodhi4 <82912690+mohsinlodhi4@users.noreply.github.com>
2021-08-18 11:00:39 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
+
+header('Content-type: text/html; charset=utf-8');
 header("ACCESS-CONTROL-ALLOW-ORIGIN: *");
+header("ACCESS-CONTROL-ALLOW-METHODS: POST");
 
 require_once __DIR__ . '/Facebook/autoload.php';
 
